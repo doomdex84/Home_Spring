b7f88194a92a6240f7d472486638db90f48d0f38
doomdex84 <doomdex84@gmail.com>
2025-05-07 20:17:55 +0900

    doDelete

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -18,18 +18,60 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		articles = new ArrayList<>();
 		lastArticleId = 0;
+		
+		makeTestDate();
+	}
+	
+	private void makeTestDate() {
+		for(int i = 0; i<=10; i++) {
+			String title = "제목"+1;
+			String body = "내용"+1;
+			
+			writeArticle(title,body);
+		}
 	}
 
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) { 
-
+	private Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
 
 		Article article = new Article(id, title, body);
 		articles.add(article);
 
 		lastArticleId++;
+		
+		return article;
+	}
+
+	private Article getArticleById(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) { 
+
+		Article article = getArticleById(id);
+		
+		if(article == null) {
+			return id + "번 글이 없음";
+		}
+		articles.remove(article);
+
+		return article;
+	}
+	
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) { 
+
+		Article article = writeArticle(title, body);
 
 		return article;
 	}
